e1e2ce311a0a2387c04ce961934f3db5a71b5cd2
Hari Kiran Yalavarthi <harikiranyalavarthi@qasymphony.com>
2019-02-06 17:53:46 -0500

    Added logging and commandline arguments

--- utility.js
+++ utility.js
@@ -2,13 +2,14 @@ const fs = require('fs');
 const csv = require('csv-parser');
 const axios = require('axios');
 
-const token = '88fc8cfd-8ff0-465b-acb2-851017fa4798'
-
-const config = {
-    headers: {'Authorization': "bearer " + token}
-};
-
-fs.createReadStream('./data.csv')
+if(process.argv.length != 6) {
+    console.log(`Please provide Token and URL`);
+}
+else {
+    const config = {
+        headers: {'Authorization': "bearer " + process.argv[3]}
+    };
+    fs.createReadStream('./sample_data.csv')
     .pipe(csv())
     .on('data', async (data) => {
         let body = {
@@ -21,8 +22,10 @@ fs.createReadStream('./data.csv')
             }
         }
         try {
-            let response = await axios.post('http://linux930.qasymphony.support:8080/api/v3/users/'+data.user_id+'/projects', body, config) 
-            console.log(response.status);
+            let response = await axios.post(process.argv[5]+'/api/v3/users/'+data.user_id+'/projects', body, config) 
+            if(response.status === 201) {
+                console.log(`Assigned User: ${data.user_id} to Project Id: ${data.project_id} as ${data.profile_name}`);
+            }
         } catch(error) {
             if(error.status === 404) {
                 console.log("The requested resource doesn\'t exist.");
@@ -30,6 +33,10 @@ fs.createReadStream('./data.csv')
                 console.log("Unauthorized");
             } else if(error.status === 400) {
                 console.log("Bad request, often due to missing a required parameter.");
+            } else if(error.errno == 'ECONNREFUSED'){
+                console.log(`It's an invalid qTest URL: ${error.errno} - Connection refused by server.`);
+            } else if(error.response.data.error == 'invalid_token' || error.response.status === 401) {
+                console.log(`It's an ${error.response.data.error_description}`);
             } else {
                 console.log(error);
             }
@@ -38,3 +45,5 @@ fs.createReadStream('./data.csv')
     .on('end', () => {
         console.log("Completed reading CSV file!");
     });
+}
+
